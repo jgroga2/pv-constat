78f6c9a8f75e8f7f58390af656030294f469cbfa
jgroga2 <jgroga2@gmail.com>
2026-09-06 15:33:25 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'pv-constat-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './app.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
